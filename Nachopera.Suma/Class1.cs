a353d773319d417508d10ee25be381a6d9cc528a
PC Correa <PC Correa@DESKTOP-Q9TQIF7>
2022-09-14 22:22:20 -0500

    Agregar archivos de proyecto.

---
+++ Nachopera.Suma/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nachopera.Suma
+{
+    public class Suma
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="j"></param>
+        /// <returns></returns>
+        public static long Sumar(long i, long j)
+        {
+            return (i + j);
+        }
+    }
+}
